b66bcceb0de8399f9a96f042975f1698dbb56353
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-11-12 19:15:46 -0500

    completed lab took a long time

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    sequence = []
+    if length > 0:
+        sequence.append(0)
+        if length > 1:
+            sequence.append(1)
+            if length > 2:
+                x = 2
+                while x < length:
+                    sequence.append(
+                        sequence[x-1] + sequence [x-2]
+                    )
+                    x += 1
+    print (sequence)
     pass
